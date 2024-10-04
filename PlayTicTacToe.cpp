21fd4386c9a28e3999e9c3e8afbaa95e41ec80a5
Liza-Sokolova <73498918+Liza-Sokolova@users.noreply.github.com>
2024-10-04 15:28:54 -0700

    Created separate main files for testing the TicTacToe object and for the main playing of the game

---
+++ PlayTicTacToe.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+* Elizaveta (Liza) Sokolova
+* PlayTicTacToe 
+* Plays a game of TicTacToe
+******************************************************************************/
+
+#include<iostream> 
+#include"TicTacToe.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+
+
+  return 0; 
+}
